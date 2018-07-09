950b51b22ddae0211c9b39614ef30d582181d32f
astridstr <astridstr24@gmail.com>
2018-07-09 13:23:46 +0700

    exprofile 20%

--- app/Http/Controllers/PDFGenerateController.php
+++ app/Http/Controllers/PDFGenerateController.php
@@ -17,7 +17,7 @@ class PdfGenerateController extends Controller
             ->where('NIP', $user->email)
             ->select('*')
             ->first(); 
-        dd($exprofile);
+        // dd($exprofile);
         PDF::setOptions(['dpi' => 150, "default_paper_size" => "a4", "default_font" => "verdana"]);
         $pdf = PDF::loadView('pdf.exprofile', ['exprofile' => $exprofile])->setPaper('a4', 'landscape');
         // return $pdf->download('ExecutiveProfile.pdf');
