aa4a65f710e5d4eed059f2508374d1bd19866189
astridstr <astridstr24@gmail.com>
2018-07-27 08:39:59 +0700

    unit

--- resources/views/unit/monitoring/app.blade.php
+++ resources/views/unit/monitoring/app.blade.php
@@ -38,27 +38,7 @@
       <span class="logo-lg"><b>PLN</b>Pusat</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- Control Sidebar Toggle Button -->
-          <li>
-             <a class="dropdown-item" href="#">Selamat Datang, {{Auth::user()->email}}</a>
-          </li>
-          <li>
-             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-               @csrf
-             </form>
-          </li>
-        </ul>
-      </div>
-    </nav>  
+    @include('unit.nav')  
   </header>
   <!-- Left side column. contains the logo and sidebar -->
   @include('unit.sidebar')
